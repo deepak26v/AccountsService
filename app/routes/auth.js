8ccea5733d6285270415ed8e5979647ac9f5ca64
Rohit Sharma <rs@gmail.com>
2018-05-20 13:04:28 -0400

    Logic to get access token

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -7,10 +7,10 @@ var authRouter = express.Router();
 authRouter.get('/start/oauth', function (req, res, next) {
     var institutionBaseUrl = configUtil.get(req.app, false, 'institutions:nordea:baseUrl');
     var clientId = configUtil.get(req.app, true, 'my_accounts_app:client_id');
-    var redirect_uri = configUtil.get(req.app, true, 'my_accounts_app:client_secret');
-    var params = { client_id: clientId, state: '', redirect_uri: redirect_uri}
-    
-    request({ url: institutionBaseUrl + '/v1/authentication', qs: params, method: 'GET' }, function (err, response) {
+    var redirect_url = configUtil.get(req.app, true, 'my_accounts_app:redirect_url');
+    var params = { client_id: clientId, state: '', redirect_uri: redirect_url};
+
+    request({ url: institutionBaseUrl, qs: params, method: 'GET' }, function (err, response) {
         if (err == null) {
             res.setHeader('Content-Type', 'application/json');
             if(response != null) {
@@ -18,6 +18,29 @@ authRouter.get('/start/oauth', function (req, res, next) {
             }
         } else {
             res.send(null);
+        }
+    });
+});
+
+authRouter.get('/authentication/get_access_token', function (req, res, next) {
+    var institutionBaseUrl = configUtil.get(req.app, false, 'institutions:nordea:baseUrl');
+    var clientId = configUtil.get(req.app, true, 'my_accounts_app:client_id');
+    var clientSecret = configUtil.get(req.app, true, 'my_accounts_app:client_secret');
+    var redirect_url = configUtil.get(req.app, true, 'my_accounts_app:redirect_url');
+    var code = req.get('code');
+
+    request({
+        url: institutionBaseUrl + '/access_token',
+        headers: {'X-IBM-Client-ID' : clientId, 'X-IBM-Client-Secret' : clientSecret},
+        form: {
+            code: code,
+            redirect_uri: redirect_url
+        },
+        method: 'POST'
+    }, function (err, response) {
+        if(err == null) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.parse(response.body));
         }
     });
 });
